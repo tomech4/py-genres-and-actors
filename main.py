0c5adc130dc8b799ff2386b022c06429f9f0f7d5
tomech <109527747+tomech4@users.noreply.github.com>
2026-02-28 22:30:55 +0100

    implement actions with database via orm as per task

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ import init_django_orm  # noqa: F401
 
 from django.db.models import QuerySet
 
+from db.models import Genre, Actor
+
 
 def main() -> QuerySet:
-    pass
+    # populate tables with data
+    genres = ["Western", "Action", "Dramma"]
+    for genre in genres:
+        Genre.objects.create(name=f"{genre}")
+
+    actors = ["George Klooney", "Kianu Reaves", "Scarlett Keegan", "Will Smith", "Jaden Smith", "Scarlett Johansson"]
+    for actor in actors:
+        first_last_name = actor.split(" ")
+        Actor.objects.create(
+            first_name=f"{first_last_name[0]}",
+            last_name=f"{first_last_name[1]}"
+        )
+
+    # update data
+    Genre.objects.get(name="Dramma").update(name="Drama")
+    Actor.objects.filter(first_name="George", last_name="Klooney").update(last_name="Clooney")
+    Actor.objects.filter(first_name="Kianu", last_name="Reaves").update(first_name="Keanu", last_name="Reeves")
+
+    # delete data
+    Genre.objects.get(name="Action").delete()
+    Actor.objects.filter(first_name="Scarlett").delete()
+
+    # return QuerySet
+    return Actor.objects.filter(last_name="Smith").order_by("first_name")
